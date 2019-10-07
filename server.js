b6eba450ae37dc502d2b34c2dad7904d093e041a
Brandon-Harris <brandonharris177@gmail.com>
2019-10-07 14:38:10 -0600

    post working

--- server.js
+++ server.js
@@ -28,6 +28,24 @@ server.get('/:id', validateAccountId, (req, res) => {
     
 });
 
+server.post('/', validateAccount, (req, res) => {
+    db('accounts').insert(req.body, 'id')
+    .then(id => {
+        console.log(`id`, id)
+        db.select('*').from('accounts')
+        .where('id', '=', id[0])
+        .then(account => {
+            console.log(`account`, account)
+            res.status(201).json(account)
+        }).catch(error => {
+            res.status(500).json(error)
+        })
+    }).catch(error => {
+        res.status(500).json(error)
+    })
+    
+});
+
 function validateAccountId(req, res, next) {
     db.select('*').from('accounts')
     .where('id', '=', req.params.id)
@@ -41,6 +59,19 @@ function validateAccountId(req, res, next) {
     }).catch (error =>
         res.status(500).json({error: `Server error: ${error}`})
     )
+};
+
+function validateAccount(req, res, next) {
+    if (req.body) {
+        if (req.body.name && req.body.budget) {
+            next ();
+        } else {
+            res.status(400).json({ message: "missing required name or budget field"  })
+        }
+    } else {
+        res.status(400).json({ message: "missing account data" })
+    }
+    
 };
 
 module.exports = server;
