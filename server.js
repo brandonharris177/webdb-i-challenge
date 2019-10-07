f86204ca655c23e7e729c6de3f48098fd7285a4e
Brandon-Harris <brandonharris177@gmail.com>
2019-10-07 14:10:27 -0600

    get by id working

--- server.js
+++ server.js
@@ -16,4 +16,31 @@ server.get('/', (req, res) => {
     
 });
 
+server.get('/:id', validateAccountId, (req, res) => {
+    db.select('*').from('accounts')
+    .where('id', '=', req.params.id)
+    .first()
+    .then(account => {
+        res.status(200).json(account)
+    }).catch(error => {
+        res.status(500).json(error)
+    })
+    
+});
+
+function validateAccountId(req, res, next) {
+    db.select('*').from('accounts')
+    .where('id', '=', req.params.id)
+    .first()
+    .then(account => {
+        if(account) {
+            next();
+        } else {
+            res.status(404).json({Messgae: "invalid accountt id"})
+        }
+    }).catch (error =>
+        res.status(500).json({error: `Server error: ${error}`})
+    )
+};
+
 module.exports = server;
